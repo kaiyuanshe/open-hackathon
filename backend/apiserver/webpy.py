385c0323f2d960c391ca550cc3a9e96d31e22fca
kehl <kangjihua_home@126.com>
2014-09-24 17:10:27 +0800

    eliminate GET method, use POST method

--- backend/apiserver/webpy.py
+++ backend/apiserver/webpy.py
@@ -1,4 +1,4 @@
-import web,json,restserver,orm,constants
+import web,restserver,orm,constants
 from backendlogger.logger import Logger
 urls = ('/(.*)', 'index')
 #orm.init_session(constants.database_str)
@@ -12,7 +12,23 @@ class index:
         
     def parse(self,uri):
         pass
-
+    
+    def POST(self,name):
+        print name
+        data = web.input()
+        request_type = data['request']
+        client_id = data['client_id']
+        image = data['image']
+        protocol = data['protocol']
+        if request_type=='1':
+            client = restserver.get_guacamole_client(client_id,image,protocol,self.mylog)
+            return client
+        elif request_type=='2':
+            restserver.shutdown_guacamole_client(client_id,image,protocol,self.mylog)
+            return None
+        else:
+            return 'Illegal Request...'
+    
     def GET(self,info):
         web.input(info=None)
         #shit=web.ctx.items()
@@ -27,20 +43,6 @@ class index:
             restserver.heart_beat(info[1],info[2])
         elif info[0]=='3':#reset guacamole client
             restserver.shutdown_guacamole_client(info[1], info[2],info[3],self.mylog)
-        '''
-        if result==1:
-            pyDict = rs.get_virtual_machine_by_lab(info[1])
-            web.header('Content-Type', 'application/json')
-            return json.dumps(pyDict)
-        elif result==2:
-            pass
-        elif result==3:
-            pass
-        elif result==4:
-            return rs.fuck
-        else:
-            return 'Illegal Request'
-        '''
         return client
 
 if __name__ == "__main__":
