bc61ded586f2a3cae6edabdb3eda4d64f3af3a18
kehl <kangjihua_home@126.com>
2014-10-31 09:18:15 +0800

    .

--- database_init.py
+++ database_init.py
@@ -1,5 +1,8 @@
-import backend.constants as constants,os
-import backend.orm.tables
+import os,sys
+sys.path.append(os.getcwd()+'/backend/')
+import constants
+import orm.tables
+
 os.system('apt-get -y install mysql-server python-mysqldb')
 if constants.database_port!=None:
     mysql_cmd = 'mysql -h'+constants.database_url +' -P' + str(constants.database_port)+' -u'+constants.database_username + ' -p'+constants.database_password
@@ -11,4 +14,4 @@ create_database = 'create database IF NOT EXISTS '+constants.database_database
 cmd = 'echo '+create_database +' | ' + mysql_cmd
 print cmd
 os.system('echo '+create_database +'|' + mysql_cmd)
-backend.orm.tables.create_tables(constants.database_str, False)
+orm.tables.create_tables(constants.database_str, False)
