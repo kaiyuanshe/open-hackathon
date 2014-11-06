5c3e5e1b605b45b1483114fa7d6c4dd9786c9cea
Junbo Wang <juniwang@microsoft.com>
2014-11-06 09:18:05 +0800

    add more dummy courses and search API

--- osslab/src/app/routes.py
+++ osslab/src/app/routes.py
@@ -3,6 +3,7 @@ __author__ = "Junbo Wang"
 from flask import render_template
 from flask.ext.restful import reqparse, abort, Api, Resource
 import json
+from osscourses import Sample_Courses
 
 Template_Routes = {
     "index": "index.html",
@@ -12,96 +13,6 @@ Template_Routes = {
     "google": "google.html"
 }
 
-Courses = [
-    {
-        "id": "flask",
-        "name": "Python+Flask",
-        "create_time": "2014-10-25",
-        "like_count": 330,
-        "description": "Python on Flask",
-        "tags": ["python", "flask", "web", "web framework", "python web framework"]
-    },
-    {
-        "id": "django",
-        "name": "Python+Django",
-        "create_time": "2014-10-25",
-        "like_count": 10,
-        "description": "Python on Django",
-        "tags": ["python", "django", "web", "web framework", "python web framework", "MVC", "mvc framework"]
-    },
-    {
-        "id": "pyramid",
-        "name": "Python+Pyramid",
-        "create_time": "2014-10-25",
-        "like_count": 320,
-        "description": "Python on Pyramid",
-        "tags": ["python", "pyramid", "web", "web framework", "python web framework"]
-    },
-    {
-        "id": "tornado",
-        "name": "Python+Tornado",
-        "create_time": "2014-10-25",
-        "like_count": 130,
-        "description": "Python on Tornado",
-        "tags": ["python", "tornado", "web", "web framework", "python web framework"]
-    },
-    {
-        "id": "beego",
-        "name": "Go+Beego",
-        "create_time": "2014-10-25",
-        "like_count": 303,
-        "description": "GO on Beego",
-        "tags": ["go", "golang", "beego", "web framework", "golang web framework", "web"]
-    },
-    {
-        "id": "rails",
-        "name": "Ruby+Rails",
-        "create_time": "2014-10-25",
-        "like_count": 23,
-        "description": "Ruby on Rails",
-        "tags": ["ruby", "rails", "web", "web framework", "ruby web framework", "MVC", "mvn framework"]
-    },
-    {
-        "id": "sinatra",
-        "name": "Ruby+Sinatra",
-        "create_time": "2014-10-25",
-        "like_count": 30,
-        "description": "Ruby on Sinatra",
-        "tags": ["ruby", "sinatra", "web", "web framework", "ruby web framework"]
-    },
-    {
-        "id": "MEAN",
-        "name": "Python+Flask",
-        "create_time": "2014-10-25",
-        "like_count": 30,
-        "description": "Python on Flask",
-        "tags": ["python", "flask", "web", "web framework", "python web framework"]
-    },
-    {
-        "id": "flask",
-        "name": "Python+Flask",
-        "create_time": "2014-10-25",
-        "like_count": 30,
-        "description": "Python on Flask",
-        "tags": ["python", "flask", "web", "web framework", "python web framework"]
-    },
-    {
-        "id": "flask",
-        "name": "Python+Flask",
-        "create_time": "2014-10-25",
-        "like_count": 30,
-        "description": "Python on Flask",
-        "tags": ["python", "flask", "web", "web framework", "python web framework"]
-    },
-    {
-        "name": "ruby on rails",
-        "create_time": "2014-10-25",
-        "like_count": 32,
-        "description": "Ruby on Rails",
-        "tags": ["ruby", "mvc", "rails", "web"]
-    }
-]
-
 def simple_route(path):
     if Template_Routes.has_key(path):
         return render_template(Template_Routes[path])
@@ -110,4 +21,13 @@ def simple_route(path):
 
 class CourseList(Resource):
     def get(self):
-        return json.dumps(Courses)
+        parser = reqparse.RequestParser()
+        parser.add_argument('tag', default=None)
+        args = parser.parse_args()
+
+        kw = args['tag']
+        if kw is None:
+            return json.dumps(Sample_Courses)
+        else:
+            ret = filter(lambda c: len(filter(lambda t: kw.lower() in t.lower(), c["tags"])) > 0, Sample_Courses)
+            return json.dumps(ret)
