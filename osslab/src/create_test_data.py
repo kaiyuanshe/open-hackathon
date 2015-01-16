b59d4be3fc31980b012ed46e88c48f61a044c62b
fendoe <ifendoe@gmail.com>
2015-01-16 11:13:17 +0800

    test

--- osslab/src/create_test_data.py
+++ osslab/src/create_test_data.py
@@ -1,5 +1,5 @@
 from app.database import *
 
-localhost = HostServer("localhost", "localhost", 8001, "10.0.2.15", 8001, 0, 100)
+localhost = HostServer("localhost", "osslab.cloudvm.cn", 8001, "10.0.2.15", 8001, 0, 100)
 db.session.add(localhost)
 db.session.commit()
