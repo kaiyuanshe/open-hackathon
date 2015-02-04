be95ea16af43189b0bbaeb1d70799faa9a584d05
Boli Guan <ifendoe@gmail.com>
2015-02-04 19:05:27 +0800

    update tempui

--- open-hackathon-tempUI/src/hackathon/static/js/hackathon.js
+++ open-hackathon-tempUI/src/hackathon/static/js/hackathon.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+
+    var ifrem = $();
+
     var cid = getParameterByName("cid");
     if (!cid || typeof(cid) == "undefined")
-        cid = "ubuntu"
+        cid = "ut"
     var main = $("#hack_main").on('mouseover', 'iframe', function(e) {
         $(this).focus();
     });
@@ -11,18 +14,32 @@ $(document).ready(function() {
         if (timer) {
             $timer.text(timerTmpe.format(timer))
         } else {
-            $('#timer').text('本次活动以结束，非常感谢您的参与。')
+            $('#timer').text('本次活动已结束，非常感谢您的参与。')
         }
     })
-    var hackathon = CONFIG.hackathon.name
+    api_stat(function(data) {
+        var str = '  <h4 class="text-center">在线人数：<span>{online} </span> 参赛人数：<span>{total}</span></h4>';
+        $('#stat').html(str.format(data));
+    })
+    $('.nav a[data-action="fullscreen"]').bind('click', function(e) {
+        $('.center iframe[class!="invisible"]').addClass('work-full')
+    });
+    $(document, 'html,body,iframe').keydown(function(e) {
+        e = window.event || e || e.which;
+        if (e.ctrlKey && e.which == 113) {
+            ifrem.toggleClass('work-full');
+            e.preventDefault()
+            return false;
+        }
+    });
 
-    function checkstart(id,callback) {
+    function checkstart(id, callback) {
         setTimeout(loopstart);
 
         function loopstart() {
             hget('/api/user/experiment?id=' + id,
                 function(data) {
-                    if (data.guacamole_status) {
+                    if (data.guacamole_servers.length > 0) {
                         callback(data)
                     } else {
                         setTimeout(loopstart, 60000);
@@ -37,7 +54,7 @@ $(document).ready(function() {
 
     hpost('/api/user/experiment', {
             "cid": cid,
-            "hackathon": hackathon
+            "hackathon": CONFIG.hackathon.name
         },
         function(resp) {
             var data = resp
@@ -49,45 +66,39 @@ $(document).ready(function() {
                             <h4>{name}<h4>\
                         </div>\
                     </div>';
-            if (servers.length > 0) {
-                checkstart(data.expr_id,function(data) {
-                    var servers = data.guacamole_servers;
-                    var work_center = $('.center');
-                    var hnav = $(".hackathon-nav").on('click', 'a.vm-box', function(e) {
-                        var a = $(this);
-                        var url = a.data('url');
-                        var name = a.attr('id')
-                        var ifrem = work_center.find('#' + name);
-                        work_center.find('iframe').css({
-                            visibility: 'hidden'
-                        });
-                        if (ifrem.length > 0) {
-                            ifrem.css({
-                                visibility: 'visible'
-                            });
-                        } else {
-                            work_center.append($('<iframe>').attr({
-                                src: url + "&token=" + get_token(),
-                                id: name,
-                                width: '100%',
-                                height: '100%',
-                                frameborder: 'yes',
-                                marginwidth: '10',
-                                scrolling: 'yes'
-                            }))
-                        }
-                    });
-                    hnav.find('a.vm-box:eq(0)').trigger('click');
-                    $.each(servers, function(i, s) {
-                        if (s.name == "Deploy") {
-                            hnav.append(tmpe.format(s));
-                        }
-                    })
-                })
-            }
+            checkstart(data.expr_id, function(data) {
+                var servers = data.guacamole_servers;
+                var work_center = $('.center');
+                var hnav = $(".hackathon-nav").on('click', 'a.vm-box', function(e) {
+                    hnav.find('.vm-box').removeClass('active')
+                    var a = $(this).addClass('active');
+                    var url = a.data('url');
+                    var name = a.attr('id')
+                    ifrem = work_center.find('#' + name);
+                    work_center.find('iframe').addClass('invisible');
+                    if (ifrem.length > 0) {
+                        ifrem.removeClass('invisible');
+                    } else {
+                        ifrem = $('<iframe>').attr({
+                            src: url + "&token=" + get_token(),
+                            id: name,
+                            width: '100%',
+                            height: '100%',
+                            frameborder: 'yes',
+                            marginwidth: '10',
+                            scrolling: 'yes'
+                        }).appendTo(work_center)
+                    }
+                });
+                $.each(servers, function(i, s) {
+                    hnav.append(tmpe.format(s));
+                });
+                hnav.find('a.vm-box:eq(0)').trigger('click');
+            })
         },
         function() {
 
         }
     );
 })
+
