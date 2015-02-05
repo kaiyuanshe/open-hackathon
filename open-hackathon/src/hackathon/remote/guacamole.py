32767e62fd460039dfb7658254c03dfbf6cc9e2e
Yifu Huang <ifhuang91@gmail.com>
2015-02-05 10:51:12 +0800

    format

--- open-hackathon/src/hackathon/remote/guacamole.py
+++ open-hackathon/src/hackathon/remote/guacamole.py
@@ -19,10 +19,10 @@ class GuacamoleInfo():
         if guacadconfig is None:
             user_template = db_adapter.find_first_object(UserTemplate, user=g.user)
             vm = db_adapter.find_first_object(UserResource,
-                                                 type=VIRTUAL_MACHINE,
-                                                 name=connection_name,
-                                                 status=RUNNING,
-                                                 user_template=user_template)
+                                              type=VIRTUAL_MACHINE,
+                                              name=connection_name,
+                                              status=RUNNING,
+                                              user_template=user_template)
             guacadconfig = db_adapter.find_first_object(VMConfig,
                                                         remote_provider=RemoteProvider.Guacamole,
                                                         virtual_machine_id=vm.id)
