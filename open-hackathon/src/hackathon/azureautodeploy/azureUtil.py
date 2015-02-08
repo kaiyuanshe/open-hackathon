398bb6ac08926e38b472d7a62709982150470f06
Junbo Wang <juniwang@microsoft.com>
2015-02-08 15:34:57 +0800

    fix python path

--- open-hackathon/src/hackathon/azureautodeploy/azureUtil.py
+++ open-hackathon/src/hackathon/azureautodeploy/azureUtil.py
@@ -1,6 +1,8 @@
 __author__ = 'Yifu Huang'
 import sys
+import os
 
+sys.path.append(os.path.dirname(__file__) + "/../../")
 sys.path.append("..")
 from hackathon.database import *
 from hackathon.database.models import *
