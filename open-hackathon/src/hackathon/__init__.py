872101c84b082c8ff1c5a72c605df7965937efb5
Junbo Wang <juniwang@microsoft.com>
2015-02-25 09:39:53 +0800

    replace slash with dot for get_config to make it more readable

--- open-hackathon/src/hackathon/__init__.py
+++ open-hackathon/src/hackathon/__init__.py
@@ -13,14 +13,12 @@ app.config['SECRET_KEY'] = '*K&ep_me^se(ret_!@#$'
 # flask restful
 api = Api(app)
 
-
 # CORS
 app.config['CORS_HEADERS'] = 'Content-Type, token'
 cors = CORS(app)
 
 
 # db configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = safe_get_config("mysql/connection", "mysql://root:root@localhost/hackathon")
+app.config["SQLALCHEMY_DATABASE_URI"] = safe_get_config("mysql.connection", "mysql://root:root@localhost/hackathon")
 
 from . import views
-import admin
---
+++ open-hackathon/src/hackathon/scheduler.py
@@ -0,0 +1,24 @@
+from functions import safe_get_config, get_config
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.mongodb import MongoDBJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+def get_job_stores():
+
+
+jobstores = {
+    'mongo': MongoDBJobStore(),
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+scheduler.start()
+
