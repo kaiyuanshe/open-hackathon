0a5b3f05690c9eed98b404ce9f868c9783c85386
Junbo Wang <juniwang@microsoft.com>
2015-03-12 15:57:16 +0800

    unit test framework: example of mock, run and result:

--- open-hackathon/test/testunit.py
+++ open-hackathon/test/testunit.py
@@ -1,24 +1,23 @@
 # A sample for unittest
 import unittest
 import sys
-sys.path.append('../src/hackathon')
-from functions import get_config
 
-#the method will be tested
+sys.path.append('../src/hackathon')
+from hackathon.functions import get_config
+
+# the method will be tested
 class FunctionsTestCases(unittest.TestCase):
     #initialize
     def setUp(self):
         self.key = 'guacamole.host'
+
     #clean up
     def tearDown(self):
         self.key = None
+
     #test case must begin with test
     def testGetConfig(self):
-        self.assertEqual("http://localhost:8080",get_config(self.key),'get_config method is not working correctly')
-
-
-
-
+        self.assertEqual("http://localhost:8080", get_config(self.key), 'get_config method is not working correctly')
 
 
 if __name__ == "__main__":
