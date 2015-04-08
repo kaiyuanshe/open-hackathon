aca9d8d7c8f4c9287c383a71badc79189a880cff
mshubian <v-bih@microsoft.com>
2015-04-08 11:07:38 +0800

    debug get one register

--- open-hackathon/src/hackathon/views.py
+++ open-hackathon/src/hackathon/views.py
@@ -273,7 +273,7 @@ class AdminRegisterResource(Resource):
         parse = reqparse.RequestParser()
         parse.add_argument('id', type=int, location='args', required=True)
         args = parse.parse_args()
-        return register_manager.get_one_register(args)
+        return register_manager.get_register_by_id(id=args['id'])
 
 
     @admin_token_required
