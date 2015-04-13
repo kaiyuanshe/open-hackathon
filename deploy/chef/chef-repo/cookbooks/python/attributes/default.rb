306b1a95658c361ddb957f20b5551370d73b649b
Junbo Wang <juniwang@microsoft.com>
2015-04-13 11:54:48 +0800

    Import python version 1.4.6

--- deploy/chef/chef-repo/cookbooks/python/attributes/default.rb
+++ deploy/chef/chef-repo/cookbooks/python/attributes/default.rb
@@ -31,9 +31,15 @@ else
   default['python']['prefix_dir']         = '/usr/local'
 end
 
-default['python']['binary'] = "#{python['prefix_dir']}/bin/python"
+default['python']['binary'] = "#{node['python']['prefix_dir']}/bin/python"
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7.1'
-default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
+default['python']['version'] = '2.7.5'
+default['python']['checksum'] = '3b477554864e616a041ee4d7cef9849751770bc7c39adaf78a94ea145c488059'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+default['python']['make_options'] = %W{install}
+
+default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
+default['python']['virtualenv_location'] = "#{node['python']['prefix_dir']}/bin/virtualenv"
+default['python']['setuptools_version'] = nil # defaults to latest
+default['python']['virtualenv_version'] = nil
