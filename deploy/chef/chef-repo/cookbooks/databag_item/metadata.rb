1fc84d847b8109d2f596d05cc98ae070974cc2a1
Junbo Wang <juniwang@microsoft.com>
2015-04-13 11:18:30 +0800

    update name/license for our own cookbooks

--- deploy/chef/chef-repo/cookbooks/databag_item/metadata.rb
+++ deploy/chef/chef-repo/cookbooks/databag_item/metadata.rb
@@ -1,10 +1,10 @@
 name             "databag_item"
-maintainer       "Autodesk"
-maintainer_email "tingjie.zhu@autodesk.com"
-license          "Apache 2.0"
+maintainer       "Microsoft Open Technologies (Shanghai) Co. Ltd"
+maintainer_email "msopentechdevsh@microsoft.com"
+license          "MIT"
 description      "Load items from databag, without worry about secrets"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 %w{ citadel }.each do |cb|
   depends cb
--- deploy/chef/chef-repo/cookbooks/open-hackathon-ui/metadata.rb
+++ deploy/chef/chef-repo/cookbooks/open-hackathon-ui/metadata.rb
@@ -1,7 +1,7 @@
-name             'front-end'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+name             'open-hackathon-ui'
+maintainer       "Microsoft Open Technologies (Shanghai) Co. Ltd"
+maintainer_email "msopentechdevsh@microsoft.com"
+license          "MIT"
 description      'Installs/Configures front-end'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
