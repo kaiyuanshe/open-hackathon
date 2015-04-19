35be49b363f6ce23714070a68a27896de51bfe2e
Junbo Wang <juniwang@microsoft.com>
2015-04-19 19:05:50 +0800

    Import forever version 0.1.1

---
+++ deploy/chef/chef-repo/cookbooks/forever/metadata.rb
@@ -0,0 +1,12 @@
+name             "forever"
+maintainer       "David Radcliffe"
+maintainer_email "david@trabian.com"
+license          "MIT"
+description      "Chef cookbook for running node apps using forever."
+long_description "See README.md"
+version          "0.1.1"
+
+supports "redhat"
+supports "centos"
+
+depends "npm"
