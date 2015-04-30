7182027b17bf6b2ae5bc54b6b7ea3636d75e0761
Guiquan Zhou <t-guzho@microsoft.com>
2015-04-30 17:54:40 +0800

    data bags

--- deploy/chef/chef-repo/cookbooks/open-hackathon-adminUI/recipes/default.rb
+++ deploy/chef/chef-repo/cookbooks/open-hackathon-adminUI/recipes/default.rb
@@ -21,6 +21,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+admin_secret = Chef::EncryptedDataBagItem.load_secret("#{node['openhackathon'][:secret][:secretpath]}")
+admin_creds = Chef::EncryptedDataBagItem.load(node.chef_environment,"secret",admin_secret)
 
 include_recipe "open-hackathon-api::source"
 
@@ -55,6 +57,14 @@ end
 
 template node['openhackathon']['admin']['src_dir']+'/app/config.py' do
   source 'config.py.erb'
+  variables(
+      :github_client_secret => admin_creds["github_client_secret"],
+      :qq_client_secret  => admin_creds["qq_client_secret"],
+      :gitcafe_client_secret => admin_creds["gitcafe_client_secret"],
+      :weibo_client_secret => admin_creds["weibo_client_secret"],
+      :app_secret => admin_creds["app_secret"],
+      :mysql_usr_pwd => admin_creds["mysql_usr_pwd"]
+    )
   owner node['openhackathon']['user']
   group node['openhackathon']['user']
   mode "0644"
