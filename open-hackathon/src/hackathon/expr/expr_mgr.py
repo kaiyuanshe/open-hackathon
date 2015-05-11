3067a695c7186e0aab1f4bbb23a1350cc5d45083
Junbo Wang <juniwang@microsoft.com>
2015-05-11 10:53:22 +0800

    fix sshd issue of ampcamp image

--- open-hackathon/src/hackathon/expr/expr_mgr.py
+++ open-hackathon/src/hackathon/expr/expr_mgr.py
@@ -2,7 +2,7 @@
 #
 # -----------------------------------------------------------------------------------
 # Copyright (c) Microsoft Open Technologies (Shanghai) Co. Ltd.  All rights reserved.
-#  
+#
 # The MIT License (MIT)
 #  
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -39,6 +39,7 @@ from hackathon.functions import safe_get_config, get_config, post_to_remote
 from subprocess import Popen
 from hackathon.scheduler import scheduler
 from datetime import timedelta
+import uuid
 
 docker = OssDocker()
 
@@ -244,7 +245,7 @@ class ExprManager(object):
     def __remote_start_container(self, expr, host_server, container_config, user_id):
         post_data = container_config
         post_data["expr_id"] = expr.id
-        post_data["container_name"] = "%s-%s" % (expr.id, container_config["name"])
+        post_data["container_name"] = "%s-%s-%s" % (expr.id, container_config["name"], str(uuid.uuid1())[0:8])
         log.debug("starting to start container: %s" % post_data["container_name"])
 
         # db entity
@@ -581,7 +582,7 @@ def open_check_expr():
 
 def check_default_expr():
     # todo only pre-allocate env for those needed. It should configured in table hackathon
-    templates = db_adapter.find_all_objects_order_by(Template, hackathon_id=2)
+    templates = db_adapter.find_all_objects_order_by(Template, hackathon_id=3)
     total_azure = safe_get_config("pre_allocate.azure", 1)
     total_docker = safe_get_config("pre_allocate.docker", 1)
     for template in templates:
