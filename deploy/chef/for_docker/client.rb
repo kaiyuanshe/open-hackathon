ec48683c5ba4785314ce35f8da65be895d3914a4
SpAiNiOr <ice_shi@163.com>
2015-05-19 15:43:55 +0800

    modify dockerfile

--- deploy/chef/for_docker/client.rb
+++ deploy/chef/for_docker/client.rb
@@ -2,5 +2,4 @@ log_location     STDOUT
 chef_server_url  "https://chef-hack.chinacloudapp.cn/organizations/msot"
 validation_client_name "msot-validator"
 validation_key "/etc/chef/msot-validator.pem"
-node_name "test1"
 trusted_certs_dir "/etc/chef/trusted_certs"
