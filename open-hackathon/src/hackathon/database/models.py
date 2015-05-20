28e4b8091112d9b3b50edc23c0618dc7894b0f12
Yifu Huang <ifhuang91@gmail.com>
2015-05-20 17:22:53 +0800

    save azure cert url

--- open-hackathon/src/hackathon/database/models.py
+++ open-hackathon/src/hackathon/database/models.py
@@ -389,9 +389,9 @@ class AzureKey(DBBase):
     __tablename__ = 'azure_key'
 
     id = Column(Integer, primary_key=True)
-    # cert file should be uploaded to azure portal
+    # cert_url is cert file path in azure
     cert_url = Column(String(200))
-    # pem file should be saved in where this program run
+    # pem_url is pem file path in local
     pem_url = Column(String(200))
     subscription_id = Column(String(100))
     management_host = Column(String(100))
