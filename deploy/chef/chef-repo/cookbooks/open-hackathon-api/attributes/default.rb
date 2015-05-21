2391ea2415fc49e74f6e9265f4b04822ebfbe878
Guiquan Zhou <t-guzho@microsoft.com>
2015-05-21 16:00:30 +0800

    modify attributes

--- deploy/chef/chef-repo/cookbooks/open-hackathon-api/attributes/default.rb
+++ deploy/chef/chef-repo/cookbooks/open-hackathon-api/attributes/default.rb
@@ -30,6 +30,8 @@ default['openhackathon'][:base_dir] = '/opt/open-hackathon'
 # scm attributes
 default['openhackathon']['git']['repository'] = 'git@github.com:msopentechcn/open-hackathon.git'
 default['openhackathon']['git']['branch'] = 'master'
+default['openhackathon']['git']['checkout_branch'] = 'deploy'
+default['openhackathon']['git']['known_hosts'] = "|1|BT8mM19S5ByEpm7VhLCODAlSlKY=|cd/jfFmnSeZi3cVRyJEJDNv5GA0= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 
 # oauth attributes
 default['openhackathon']['github']['client_id'] = ''
