a62bcf1609dfcfc1bbaad42d5de691713c7c24a8
Junbo Wang <juniwang@microsoft.com>
2015-07-03 16:55:30 +0800

    rename open-hackathon-adminUI to open-hackathon-client

---
+++ open-hackathon-client/src/client/views/route_template.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) Microsoft Open Technologies (Shanghai) Co. Ltd.  All rights reserved.
+ 
+The MIT License (MIT)
+ 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import sys
+
+sys.path.append("..")
+
+from client import app
+from . import render
+from flask_login import login_required
+
+
+@app.route("/template")
+def template_index():
+    return render("/template/index.html")
+
+
+@app.route("/template/create")
+@login_required
+def template_create():
+    return render("/template/create.html")
+
+
+@app.route("/template/create/<template_id>")
+@login_required
+def template_create_from_existing(template_id):
+    return render("/template/create.html", template_id=template_id)
+
+
+@app.route("/template/edit/<template_id>")
+def template_edit():
+    return render("/template/create.html")
+
+
+@app.route("/template/try/<template_id>")
+@login_required
+def try_template(template_id):
+    return render("/manage/testtemplate.html")
