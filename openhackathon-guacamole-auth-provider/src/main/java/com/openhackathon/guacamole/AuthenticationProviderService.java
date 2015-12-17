74fe11818b9813c9717ee7fb89ee10b66479deb4
YaningX <yaning0123@gmail.com>
2015-12-17 17:28:31 +0800

    wip

---
+++ openhackathon-guacamole-auth-provider/src/main/java/com/openhackathon/guacamole/AuthenticationProviderService.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2015 Glyptodon LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.openhackathon.guacamole;
+
+import org.glyptodon.guacamole.GuacamoleException;
+import org.glyptodon.guacamole.net.auth.AuthenticatedUser;
+import org.glyptodon.guacamole.net.auth.Connection;
+import org.glyptodon.guacamole.net.auth.Credentials;
+import org.glyptodon.guacamole.net.auth.UserContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * Service providing convenience functions for the LDAP AuthenticationProvider
+ * implementation.
+ */
+public class AuthenticationProviderService {
+    /**
+     * Logger for this class.
+     */
+    private final Logger logger = LoggerFactory.getLogger(AuthenticationProviderService.class);
+
+    /**
+     * Returns an AuthenticatedUser representing the user authenticated by the
+     * given credentials.
+     *
+     * @param credentials
+     *     The credentials to use for authentication.
+     *
+     * @return
+     *     An AuthenticatedUser representing the user authenticated by the
+     *     given credentials.
+     *
+     * @throws GuacamoleException
+     *     If an error occurs while authenticating the user, or if access is
+     *     denied.
+     */
+    public AuthenticatedUser authenticateUser(Credentials credentials)
+            throws GuacamoleException {
+
+
+        return null;
+    }
+
+
+    /**
+     * Returns a UserContext object initialized with data accessible to the
+     * given AuthenticatedUser.
+     *
+     * @param authenticatedUser
+     *     The AuthenticatedUser to retrieve data for.
+     *
+     * @return
+     *     A UserContext object initialized with data accessible to the given
+     *     AuthenticatedUser.
+     *
+     * @throws GuacamoleException
+     *     If the UserContext cannot be created due to an error.
+     */
+    public UserContext getUserContext(org.glyptodon.guacamole.net.auth.AuthenticatedUser authenticatedUser)
+            throws GuacamoleException {
+
+        return null;
+    }
+
+}
--- openhackathon-guacamole-auth-provider/src/main/java/com/openhackathon/guacamole/OpenHackathonAuthenticationProvider.java
+++ openhackathon-guacamole-auth-provider/src/main/java/com/openhackathon/guacamole/OpenHackathonAuthenticationProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Microsoft Open Technologies (Shanghai) Co. Ltd.  All rights reserved. 
+ * Copyright (c) Microsoft Open Technologies (Shanghai) Co. Ltd.  All rights reserved.
  *
  * The MIT License (MIT)
  *
@@ -21,109 +21,68 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package com.openhackathon.guacamole;
 
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.glyptodon.guacamole.GuacamoleException;
+import org.glyptodon.guacamole.net.auth.AuthenticatedUser;
+import org.glyptodon.guacamole.net.auth.AuthenticationProvider;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.UserContext;
-import org.glyptodon.guacamole.net.auth.simple.SimpleAuthenticationProvider;
-import org.glyptodon.guacamole.net.auth.simple.SimpleConnection;
-import org.glyptodon.guacamole.net.auth.simple.SimpleConnectionDirectory;
-import org.glyptodon.guacamole.properties.StringGuacamoleProperty;
-import org.glyptodon.guacamole.properties.GuacamoleProperties;
-import org.glyptodon.guacamole.protocol.GuacamoleConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+public class OpenHackathonAuthenticationProvider implements AuthenticationProvider {
+    /**
+     * The identifier reserved for the root connection group.
+     */
+    public static final String ROOT_CONNECTION_GROUP = "ROOT";
 
+    /**
+     * Injector which will manage the object graph of this authentication
+     * provider.
+     */
+    private final Injector injector;
 
-public class OpenHackathonAuthenticationProvider extends SimpleAuthenticationProvider {
+    /**
+     * Creates a new AWSAuthenticationProvider that pulls user connection data
+     * from AWS.
+     *
+     * @throws GuacamoleException
+     *     If a required property is missing, or an error occurs while parsing
+     *     a property.
+     */
+    public OpenHackathonAuthenticationProvider() throws GuacamoleException {
 
-    private final Logger logger = LoggerFactory.getLogger(OpenHackathonAuthenticationProvider.class.getClass());
-    private Connect2OpenHackathon conn;
-    private final Trans2GuacdConfiguration trans = new Trans2GuacdConfiguration();
-
-    private static final StringGuacamoleProperty AUTH_REQUEST_URL = new StringGuacamoleProperty() {
-        @Override
-        public String getName() {
-            return "auth-request-url";
-        }
-    };
-
-    public OpenHackathonAuthenticationProvider() {
-        initConnection();
-        logger.info("initialize OpenHackathonAuthenticationProvider");
-    }
-
-    @Override
-    public Map<String, GuacamoleConfiguration> getAuthorizedConfigurations(final Credentials credentials) throws GuacamoleException {
-
-        initConnection();
-
-        Map<String, GuacamoleConfiguration> configs = new HashMap<String, GuacamoleConfiguration>();
-
-        final GuacamoleConfiguration config = getGuacamoleConfiguration(credentials.getRequest());
-        if (config == null) {
-            return configs;
-        }
-
-        configs.put(config.getParameter("name"), config);
-        return configs;
+        // Set up Guice injector.
+        injector = Guice.createInjector(
+                new OpenHackthonAuthenticationProviderModule(this)
+        );
     }
 
 
     @Override
-    public UserContext updateUserContext(final UserContext context, final Credentials credentials) throws GuacamoleException {
-        final HttpServletRequest request = credentials.getRequest();
-        final GuacamoleConfiguration config = getGuacamoleConfiguration(request);
+    public String getIdentifier() {
+        return "openhackthon";
+    }
 
-        if (config == null) {
-            return context;
-        }
+    @Override
+    public AuthenticatedUser authenticateUser(Credentials credentials) throws GuacamoleException {
+        return null;
+    }
 
-        final String name = config.getParameter("name");
-        logger.info("Instance Type of ConnectionDirectory is: " + context.getConnectionDirectory().getClass().getName());
-        final SimpleConnectionDirectory connections = (SimpleConnectionDirectory) context.getConnectionDirectory();
-        logger.info("get info from GuacamoleConfiguration name:" + name);
-        logger.info("protocol select :" + config.getProtocol());
-        final SimpleConnection connection = new SimpleConnection(name, name, config);
-        connections.putConnection(connection);
+    @Override
+    public AuthenticatedUser updateAuthenticatedUser(AuthenticatedUser authenticatedUser, Credentials credentials) throws GuacamoleException {
+        return authenticatedUser;
+    }
+
+    @Override
+    public UserContext getUserContext(AuthenticatedUser authenticatedUser) throws GuacamoleException {
+        return null;
+    }
+
+    @Override
+    public UserContext updateUserContext(UserContext context, AuthenticatedUser authenticatedUser) throws GuacamoleException {
         return context;
-    }
-
-    private GuacamoleConfiguration getGuacamoleConfiguration(final HttpServletRequest request) throws GuacamoleException {
-
-        final String tokenString = request.getParameter("oh");
-        final String connectionName = request.getParameter("name");
-        logger.info("open hackathon tokenString is : " + tokenString + ", connectionName is:" + connectionName);
-
-        if(tokenString == null || connectionName==null){
-            return null;
-        }
-
-
-        final String jsonString = this.conn.getGuacamoleJSONString(connectionName, tokenString);
-        logger.info("get guacamole config json String :" + jsonString);
-        if (jsonString == null) {
-            logger.info("get null jsonString from openHackathon platform");
-            return null;
-        }
-
-        // String finalString = jsonString.substring(1, jsonString.length()-1).replace("\\", "");
-        return trans.getConfiguration(jsonString);
-    }
-
-    private synchronized void initConnection() {
-        if (conn != null)
-            return;
-        try {
-            final String authRequestURL = GuacamoleProperties.getProperty(AUTH_REQUEST_URL);
-            this.conn = new Connect2OpenHackathon(authRequestURL);
-        } catch (GuacamoleException e) {
-            logger.error("fail to get AUTH_REQUEST_URL from config file", e);
-        }
     }
 }
