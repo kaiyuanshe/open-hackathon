2db3a0a5ebf538923c09010a9dce5dc895da7e97
rapidhere <rapidhere@gmail.com>
2016-01-01 14:56:59 +0800

    update test

--- open-hackathon-server/test/unittest/hazure/test_conf.py
+++ open-hackathon-server/test/unittest/hazure/test_conf.py
@@ -35,10 +35,11 @@ subscription_id = "bc41bdcd-7387-483f-a929-488fa27616a0"
 pem_url = "/home/rapid/tmp/azure-test.pem"
 management_host = "management.core.chinacloudapi.cn"
 
+# general date for all test cases
 meanless_id = "-233"
 meanless_name = "random-meanless-name-" + str(random.random())
 
-# use for real test
+# used for real test cases
 azure_cloud_service_name_should_exist = "ampcamp00"
 azure_cloud_service_to_create = {
     "name": "rapid-test",
