794f7cf81fac51e148b240ae40e387670a6c916a
fendoe <ifendoe@gmail.com>
2016-03-24 17:58:33 +0800

    update

--- open-hackathon-server/src/hackathon/azureformation/service.py
+++ open-hackathon-server/src/hackathon/azureformation/service.py
@@ -40,15 +40,11 @@ from hackathon.azureformation.utility import (
     run_job,
 )
 
-<<<<<<< HEAD
-# from hackathon.database.models import (
-#     AzureKey,
-# )
-=======
+
 from hackathon.hmongo.models import (
     AzureKey,
 )
->>>>>>> junbo/master
+
 from azure.servicemanagement import (
     ServiceManagementService,
     Deployment,
--- open-hackathon-server/src/hackathon/azureformation/utility.py
+++ open-hackathon-server/src/hackathon/azureformation/utility.py
@@ -27,24 +27,7 @@ __author__ = 'Yifu Huang'
 import sys
 
 sys.path.append("..")
-<<<<<<< HEAD
-# from hackathon.database import (
-#     db_adapter,
-# )
-# from hackathon.database.models import (
-#     AzureLog,
-#     AzureStorageAccount,
-#     AzureCloudService,
-#     AzureDeployment,
-#     AzureVirtualMachine,
-#     AzureEndpoint,
-#     VirtualEnvironment,
-#     Template,
-#     Experiment,
-# )
-=======
 
->>>>>>> junbo/master
 from hackathon.util import (
     load_template,
     call,
