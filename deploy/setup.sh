0e649a02fbcbbe86b9ec920adc22a6f0bc1b87b4
YaningX <yaning0123@gmail.com>
2016-03-31 11:51:47 +0800

    wip

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -6,11 +6,13 @@ if grep -q "Could not resolve" <<< $result; then
     echo "Could not update apt-get, please solve it"
     exit
 fi
+echo "installing git python-setuptools python-dev python-pip"
 result=$(sudo apt-get install -y git python-setuptools python-dev python-pip)
 if grep -q "Unable to lacate" <<< $result; then
     echo "Could not install dependancy software, pls install docker manually"
     exit
 fi
+echo "install autoconf libtool tomcat7"
 result=$(sudo apt-get install -y autoconf libtool tomcat7)
 if grep -q "Unable to lacate" <<< $result; then
     echo "Could not install dependancy software, pls install docker manually"
@@ -21,12 +23,14 @@ fi
 set_envirement() {
 cd /home && sudo rm -rf opentech
 sudo mkdir opentech && cd opentech
+echo "git cloning open-hackathon source code"
 result=$(sudo git clone https://github.com/msopentechcn/open-hackathon.git)
 if grep -q "unable to access" <<< $result; then
     echo "Could not git clone open-hackathon source code, pls check your network"
     exit
 fi
 cd /home/opentech/open-hackathon
+echo "installing required python library"
 sudo pip install -r open-hackathon-server/requirement.txt
 sudo pip install -r open-hackathon-client/requirement.txt
 sudo cp open-hackathon-server/src/hackathon/config_sample.py open-hackathon-server/src/hackathon/config.py
@@ -34,22 +38,30 @@ sudo cp open-hackathon-server/src/hackathon/config_sample.py open-hackathon-serv
 }
 
 get_dependency_for_guacamole() {
-result=$(sudo apt-get install -y libcairo2-dev libjpeg62-turbo-dev)
+echo "installing libcairo2-dev"
+result=$(sudo apt-get install -y libcairo2-dev)
 if grep -q "Unable to lacate" <<< $result; then
-    echo "Could not install dependancy software for guacamole, pls install docker manually"
+    echo "Could not install dependancy software for guacamole, pls install guacamole manually"
     exit
 fi
+echo "installing libjpeg62-dev libpng12-dev libossp-uuid-dev"
 result=$(sudo apt-get install -y libjpeg62-dev libpng12-dev libossp-uuid-dev)
 if grep -q "Unable to lacate" <<< $result; then
-    echo "Could not install dependancy software for guacamole, pls install docker manually"
+    echo "Could not install dependancy software for guacamole, pls install guacamole manually"
     exit
 fi
+echo "installing libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev"
 result=$(sudo apt-get install -y libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev)
 if grep -q "Unable to lacate" <<< $result; then
-    echo "Could not install dependancy software for guacamole, pls install docker manually"
+    echo "Could not install dependancy software for guacamole, pls install guacamole manually"
     exit
 fi
-sudo apt-get install -y libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev
+echo "installing libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev"
+result=$(sudo apt-get install -y libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev)
+if grep -q "Unable to lacate" <<< $result; then
+    echo "Could not install dependancy software for guacamole, pls install guacamole manually"
+    exit
+fi
 }
 
 install_and_config_guacamole() {
@@ -86,22 +98,7 @@ if grep -q "Unable to lacate" <<< $result; then
     exit
 fi
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-#get ubuntu version
-#for ubuntu 14
-#this requires root access
-if [ $(lsb_release -d | grep -q "14") ]; then
-    sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-fi
 
-# for ubuntu 15
-if [ $(lsb_release -d | grep -q "15") ]; then
-    sudo echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" > /etc/apt/sources.list.d/docker.list
-fi
-
-# for linux mint
-if [ $(lsb_release -d | grep -q "Mint") ]; then
-    sudo echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" > /etc/apt/sources.list.d/docker.list
-fi
 result=$(sudo apt-get update)
 if grep -q "Could not resolve" <<< $result; then
     echo "Could not update apt-get, please solve it"
