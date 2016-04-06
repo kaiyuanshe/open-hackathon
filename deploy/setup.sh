fc3013e3ce44f3ede8777d88515dce158c8a9bfc
YaningX <yaning0123@gmail.com>
2016-04-06 13:24:10 +0800

    fix install docker fail

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,19 +2,19 @@
 
 get_dependency_software() {
     echo "updating apt-get......"
-    result=$( apt-get update)
+    result=$(sudo apt-get update)
     if grep -q "Could not resolve" <<< $result; then
         echo "Could not update apt-get, please solve it"
         exit
     fi
     echo "installing git python-setuptools python-dev python-pip"
-    result=$( apt-get install -y git python-setuptools python-dev python-pip)
+    result=$(sudo apt-get install -y git python-setuptools python-dev python-pip)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software, pls install docker manually"
         exit
     fi
     echo "installing autoconf libtool tomcat7 mongodb"
-    result=$( apt-get install -y autoconf automake libtool tomcat7 mongodb)
+    result=$(sudo apt-get install -y autoconf automake libtool tomcat7 mongodb)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software, pls install docker manually"
         exit
@@ -27,16 +27,16 @@ set_envirement() {
     mkdir opentech && cd opentech
     rm -r open-hackathon
     echo "git cloning open-hackathon source code"
-    result=$( git clone https://github.com/YaningX/open-hackathon.git)
+    result=$(sudo git clone https://github.com/YaningX/open-hackathon.git)
     if grep -q "unable to access" <<< $result; then
         echo "Could not git clone open-hackathon source code, pls check your network"
         exit
     fi
     cd /home/opentech/open-hackathon
     echo "pip is installing required python library"
-    result=$( pip install -r open-hackathon-server/requirement.txt)
+    result=$(sudo pip install -r open-hackathon-server/requirement.txt)
 
-    result=$( pip install -r open-hackathon-client/requirement.txt)
+    result=$(sudo pip install -r open-hackathon-client/requirement.txt)
     cp open-hackathon-server/src/hackathon/config_sample.py open-hackathon-server/src/hackathon/config.py
     cp open-hackathon-server/src/hackathon/config_sample.py open-hackathon-server/src/hackathon/config.py
 }
@@ -49,25 +49,25 @@ get_dependency_for_guacamole() {
         return
     fi
     echo "installing libcairo2-dev"
-    result=$( apt-get install -y libcairo2-dev)
+    result=$(sudo apt-get install -y libcairo2-dev)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software for guacamole, pls install guacamole manually"
         exit
     fi
     echo "installing libjpeg62-dev libpng12-dev libossp-uuid-dev"
-    result=$( apt-get install -y libjpeg62-dev libpng12-dev libossp-uuid-dev)
+    result=$(sudo apt-get install -y libjpeg62-dev libpng12-dev libossp-uuid-dev)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software for guacamole, pls install guacamole manually"
         exit
     fi
     echo "installing libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev"
-    result=$( apt-get install -y libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev)
+    result=$(sudo apt-get install -y libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software for guacamole, pls install guacamole manually"
         exit
     fi
     echo "installing libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev"
-    result=$( apt-get install -y libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev)
+    result=$(sudo apt-get install -y libvncserver-dev libpulse-dev libwebp-dev libssl-dev libvorbis-dev)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software for guacamole, pls install guacamole manually"
         exit
@@ -111,28 +111,28 @@ install_and_config_guacamole() {
 
 # install docker
 install_and_config_docker() {
-    result=$( apt-get update)
+    result=$(sudo apt-get update)
     if grep -q "Could not resolve" <<< $result; then
         echo "Could not update apt-get, please solve it"
         exit
     fi
-    result=$( apt-get install apt-transport-https ca-certificates)
+    result=$(sudo apt-get install apt-transport-https ca-certificates)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install dependancy software for docker, pls install docker manually"
         exit
     fi
 
-    result=$( apt-get update)
+    result=$(sudo apt-get update)
     if grep -q "Could not resolve" <<< $result; then
         echo "Could not update apt-get, please solve it"
         exit
     fi
-    result=$( apt-get purge lxc-docker)
+    result=$(sudo apt-get purge lxc-docker)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install docker, pls install docker manually"
         exit
     fi
-    result=$(apt-cache policy docker-engine)
+    result=$(sudo apt-cache policy docker-engine)
     # for ubuntu 15
     result=$(apt-get install -y linux-image-extra-$(uname -r))
     if grep -q "Unable to lacate" <<< $result; then
@@ -140,12 +140,12 @@ install_and_config_docker() {
         exit
     fi
     # for ubuntu 12 & 14
-    result=$(apt-get install -y apparmor)
+    result=$(sudo apt-get install -y apparmor)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install apparmor"
         exit
     fi
-    result=$(apt-get install -y docker-engine)
+    result=$(sudo apt-get install -y docker-engine)
     if grep -q "Unable to lacate" <<< $result; then
         echo "Could not install apparmor"
         exit
