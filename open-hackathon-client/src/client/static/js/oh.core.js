afe7755bb21ae891278395bf7096fe4ed75cc9f8
Boli Guan <ifendoe@gmail.com>
2016-04-08 17:14:19 +0800

    update team page

--- open-hackathon-client/src/client/static/js/oh.core.js
+++ open-hackathon-client/src/client/static/js/oh.core.js
@@ -87,7 +87,7 @@
             var parent = $(img).parents('[data-parent]');
             var FitWidth = parent.width();
             var FitHeight = parent.height();
-            var image = {width: img.width, height: img.height}
+            var image = {width: img.naturalWidth, height: img.naturalHeight}
             var height = (image.height * FitWidth) / image.width;
             var width = (image.width * FitHeight) / image.height;
             if (height > FitHeight) {
