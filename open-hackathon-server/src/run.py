1e3817eb7871ac85c46e1e8be9d50580b074bf2d
Junbo Wang <juniwang@microsoft.com>
2016-04-15 00:49:50 +0800

    restore hackathon scheduler

--- open-hackathon-server/src/run.py
+++ open-hackathon-server/src/run.py
@@ -28,4 +28,4 @@
 from hackathon import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=15000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=15000, debug=True, threaded=True, use_reloader=False)
