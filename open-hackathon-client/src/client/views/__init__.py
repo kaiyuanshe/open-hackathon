5fb5b086649caf63702335838e31c242c8670e8f
SpAiNiOr <ice_shi@163.com>
2016-05-13 16:57:29 +0800

    change get to safe_get

--- open-hackathon-client/src/client/views/__init__.py
+++ open-hackathon-client/src/client/views/__init__.py
@@ -230,7 +230,7 @@ def unauthorized_log():
 def make_session_permanent():
     g.user = current_user
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=get_config("login.session_valid_time_minutes"))
+    app.permanent_session_lifetime = timedelta(minutes=safe_get_config("login.session_valid_time_minutes", 60))
 
 
 @app.errorhandler(401)
