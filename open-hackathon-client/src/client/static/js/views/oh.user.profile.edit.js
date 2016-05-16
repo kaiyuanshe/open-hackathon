cb3b0815edac4ac9c7a491956cec02f6d0fe710a
lclchen <liuchensj@sjtu.edu.cn>
2016-05-16 06:08:39 +0100

    Support upload user_avatar

--- open-hackathon-client/src/client/static/js/views/oh.user.profile.edit.js
+++ open-hackathon-client/src/client/static/js/views/oh.user.profile.edit.js
@@ -80,6 +80,16 @@
         return oh.api.user.picture.put({body: {url: url}});
     }
 
+    function disableItems(list) {
+        for (var index in list)
+            $(list[index]).attr('disabled', true);
+    }
+
+    function enableItems(list) {
+        for (var index in list)
+            $(list[index]).removeAttr('disabled');
+    }
+
     function init() {
         pageload();
         profileFormValidator();
@@ -94,7 +104,7 @@
         var picture_form = $('#pictureForm').bootstrapValidator()
             .on('success.form.bv', function (e) {
                 e.preventDefault();
-                var picture = $('#picture').val();
+                var picture = $('#user_logo_input').val();
                 updatePicture(picture).then(function (data) {
                     if (data.error) {
                         oh.comm.alert('����', data.error.friendly_message);
@@ -114,6 +124,41 @@
         var pictureModal = $('#pictureModal').on('hide.bs.modal', function (e) {
             picture_form.get(0).reset();
             picture_form.data().bootstrapValidator.resetForm();
+        });
+
+        // upload file about user logo
+        $("#user_logo_upload").fileupload({
+            type: 'POST',
+            url: CONFIG.apiconfig.proxy + '/api/user/file?file_type=user_file',
+            dataType: 'json',
+            beforeSend: function (xhr, data) {
+                xhr.setRequestHeader('token', $.cookie('token'));
+            },
+            start: function () {
+                $('#user_logo_upload_span').text('文件上传中，上传成功将会自动跳转，请稍等...');
+                disableItems(['#user_logo_input', '#user_logo_upload_btn', '#user_logo_submit', '#user_logo_cancel']);
+            },
+            done: function (e, obj) {
+                var data = obj.result;
+                if (data.error || ! "files" in data || data.files.length == 0) {
+                    oh.comm.alert('错误', data.error.friendly_message);
+                } else {
+                    var url = data.files[0].url;
+                    oh.api.user.picture.put({body: {url: url}}).then(function (data) {
+                        if (data.error) {
+                            oh.comm.alert('错误', data.error.friendly_message);
+                        } else {
+                            $('#user_logo').attr({src: url});
+                            $('#user_logo_input').val("");
+                            $('#pictureModal').modal('hide');
+                            $('#user_logo_upload_span').text('上传文件');
+                            enableItems(['#user_logo_input', '#user_logo_upload_btn', '#user_logo_submit',
+                                         '#user_logo_cancel']);
+                            oh.comm.alert("提示", "设置成功!");
+                        }
+                    });
+                }
+            }
         });
     }
 
