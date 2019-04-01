db5cb549ac681ccb1a8dda359c5edb493da10f55
Hypo <i@ihypo.net>
2019-04-01 23:25:30 +0800

    edit k8sAdapter client init and upgrade k8s sdk version to support 1.13

--- open-hackathon-server/src/hackathon/hk8s/k8s_service_adapter.py
+++ open-hackathon-server/src/hackathon/hk8s/k8s_service_adapter.py
@@ -5,9 +5,9 @@ This file is covered by the LICENSING file in the root of this project.
 
 import sys
 
-
 from os import path
-from kubernetes import client, config, utils
+from kubernetes import client, utils
+from kubernetes.client.rest import ApiException
 
 from hackathon import RequiredFeature, Component, Context
 from hackathon.constants import HEALTH, HEALTH_STATUS, HACKATHON_CONFIG, CLOUD_PROVIDER
@@ -17,70 +17,73 @@ sys.path.append("..")
 
 __all__ = ["K8SServiceAdapter"]
 
+
 class K8SServiceAdapter(ServiceAdapter):
 
-    def __init__(self, config_file):
-        self.default_kube_config_file = "./kubeconfig.json"
+    def __init__(self):
+        # TODO add kube config
+        configuration = client.Configuration()
+        configuration.host = 'KUBE_API_SERVER'
+        configuration.api_key['authorization'] = 'bearer ' + 'SERVICE_ACCOUNT_TOKEN'
+        # FIXME import ca cert file?
+        configuration.verify_ssl = False
 
-        if config_file == None :
-            self.kube_config_file = self.default_config_file
-        else:
-            self.kube_config_file = config_file
-
-        config.load_kube_config(self.kube_config_file)
-        self.k8s_client = client.ApiClient()
+        self.api_client = client.ApiClient(configuration)
+        super(K8SServiceAdapter, self).__init__(self.api_client)
 
     def create_k8s_deployment_with_yaml(self, yaml, name, namespace):
         try:
-            self.k8s_api = utils.create_from_yaml(self.k8s_client, yaml)
-            #see https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/AppsV1Api.md#read_namespaced_deployment
-            resp = self.k8s_api.read_namespaced_deployment(name, namespace)
+            api_instance = utils.create_from_yaml(self.api_client, yaml)
+            # https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/AppsV1Api.md
+            resp = api_instance.read_namespaced_deployment(name, namespace)
             return format(resp.metadata.name)
-        except Exception as e:
-            if 'Confilct' == e.reason:
-                return name
-            else:
-                self.log.error(e)
-                return None
+        except ApiException as e:
+            self.log.error(e)
+            return None
 
     def deployment_exists(self, name):
         return name in self.list_deployments(name)
 
     def report_health(self):
-        raise NotImplementedError()
-
-
+        try:
+            api_instance = client.VersionApi(self.api_client)
+            result = api_instance.get_code()
+            assert result, "Get nothing about cluster version from apiServer."
+            return {HEALTH.STATUS: HEALTH_STATUS.OK}
+        except ApiException as e:
+            self.log.error(e)
+            return {
+                HEALTH.STATUS: HEALTH_STATUS.ERROR,
+                HEALTH.DESCRIPTION: "Get cluster info error: {}".format(e),
+            }
 
     def start_k8s_service(self, name, namespace):
         raise NotImplementedError()
 
+    def pause_k8s_service(self):
+        raise NotImplementedError()
 
     def stop_k8s_service(self):
         raise NotImplementedError()
 
-
     def ping(self, url, timeout=20):
-        pass
+        report = self.report_health()
+        return report[HEALTH.STATUS] == HEALTH_STATUS.OK
 
-#    def get_deployment_detail_by_name(self, deployment_name):
-#        return
-
-    def list_deployments(self, deployment_name, timeout=20):
-        list = []
-        v1 = client.ExtensionsV1beta1Api()
-        #see https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/AppsV1Api.md#list_deployment_for_all_namespaces
-        ret = v1.list_deployment_for_all_namespaces(watch=False)
+    def list_deployments(self, namespace=None, timeout=20):
+        _deployments = []
+        apps_v1_group = client.AppsV1Api(self.api_client)
+        if not namespace:
+            ret = apps_v1_group.list_deployment_for_all_namespaces(timeout_seconds=timeout, watch=False)
+        else:
+            ret = apps_v1_group.list_namespaced_deployment(namespace, timeout_seconds=timeout, watch=False)
 
         for i in ret.items:
-            list.append(i.metadata.name)
-
-        return list
+            _deployments.append(i.metadata.name)
+        return _deployments
 
     def get_deployment_by_name(self, deployment_name, namespace):
         raise NotImplementedError()
-        #https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/AppsV1Api.md#read_namespaced_deployment
-        #deps = self.k8s_api.read_namespaced_deployment(deployment_name, namespace)
-        #return deps.metadata.available
-
-#if __name__ == '__main__':
-#    main()
+        # https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/AppsV1Api.md#read_namespaced_deployment
+        # deps = self.k8s_api.read_namespaced_deployment(deployment_name, namespace)
+        # return deps.metadata.available
