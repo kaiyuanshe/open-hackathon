7e79b75968d86586f622d7e1a8d8fa9d6963ce66
99Kies <1290017556@qq.com>
2020-03-14 22:37:39 +0800

    update import path

--- open-hackathon-server/src/reset_db.py
+++ open-hackathon-server/src/reset_db.py
@@ -3,8 +3,8 @@
 This file is covered by the LICENSING file in the root of this project.
 """
 
-from .hackathon.hmongo import drop_db
-from .setup_db import setup_db
+from hackathon.hmongo import drop_db
+from setup_db import setup_db
 
 drop_db()
 setup_db()
