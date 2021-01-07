399a823ff28e1f5e0f4f31d947c819a5fe162fac
Junbo Wang <juniwang@microsoft.com>
2021-01-07 15:45:06 +0800

    adding DB entity for hackathon

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/Enums/EnumTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Models.Enums
+{
+    public enum HackathonStatus
+    {
+        Planning,
+        PendingApproval,
+        Online,
+        Offline
+    } 
+}
--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Entities/HackathonEntity.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Entities/HackathonEntity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kaiyuanshe.OpenHackathon.Server.Models.Enums;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +11,57 @@ namespace Kaiyuanshe.OpenHackathon.Server.Storage.Entities
     /// </summary>
     public class HackathonEntity : AdvancedTableEntity
     {
-        public string DisplayName { get; set; }
+        /// <summary>
+        /// Id of Hackathon. Auto-generated, the same as PartitionKey
+        /// </summary>
+        [IgnoreProperty]
+        public string Id
+        {
+            get
+            {
+                return PartitionKey;
+            }
+        }
+
+        public string Name { get; set; }
+        /// <summary>
+        /// A short sentence for advertisement
+        /// </summary>
+        public string Ribbon { get; set; }
+        public string Summary { get; set; }
+        /// <summary>
+        /// Detailed description. Usually rich-text html
+        /// </summary>
+        public string Description { get; set; }
+        public string Location { get; set; }
+        /// <summary>
+        /// Uploaded pictures for display as banners
+        /// </summary>
+        public string[] Banners { get; set; }
+        public HackathonStatus Status { get; set; }
+        /// <summary>
+        /// Id of User who creates this hackathon
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// Maximum allowed participant
+        /// </summary>
+        public int MaxEnrollment { get; set; }
+        /// <summary>
+        /// whether or not user registration needs admin's manual approval
+        /// </summary>
+        public bool AutoApprove { get; set; }
+        /// <summary>
+        /// an array of organizer's Id
+        /// </summary>
+        public string[] Tags { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime EventStartTime { get; set; }
+        public DateTime EventEndTime { get; set; }
+        public DateTime RegisterStartTime { get; set; }
+        public DateTime RegisterEndTime { get; set; }
+        public DateTime JudgeStartTime { get; set; }
+        public DateTime JudgeEndTime { get; set; }
+        public DateTime ArchiveTime { get; set; }
     }
 }
