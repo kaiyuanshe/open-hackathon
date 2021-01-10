38700b3c14f036bb09cbc96d5352ee683c77e01c
Junbo Wang <juniwang@microsoft.com>
2021-01-10 21:59:13 +0800

    Adding errorResponse

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/HackathonController.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/HackathonController.cs
@@ -1,4 +1,7 @@
-﻿using Kaiyuanshe.OpenHackathon.Server.Storage;
+﻿using Kaiyuanshe.OpenHackathon.Server.Biz;
+using Kaiyuanshe.OpenHackathon.Server.Models;
+using Kaiyuanshe.OpenHackathon.Server.ResponseBuilder;
+using Kaiyuanshe.OpenHackathon.Server.Storage;
 using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -9,19 +12,40 @@ namespace Kaiyuanshe.OpenHackathon.Server.Controllers
 {
     public class HackathonController : HackathonControllerBase
     {
-        public IStorageContext StorageContext { get; set; }
+        public IResponseBuilder ResponseBuilder { get; set; }
 
+        public IHackathonManager HackathonManager { get; set; }
+
+        /// <summary>
+        /// List hackathons.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A list of hackathon.</returns>
+        /// <response code="200">Success. The response describes a list of hackathon.</response>
         [HttpGet]
-        [Route("hackathon")]
-        public async Task<object> List(CancellationToken cancellationToken)
+        [ProducesResponseType(typeof(Hackathon[]), 200)]
+        [Route("hackathons")]
+        public async Task<object> ListHackathon(CancellationToken cancellationToken)
         {
-            var entities = new List<HackathonEntity>();
-            await StorageContext.HackathonTable.ExecuteQuerySegmentedAsync(null, (segment) =>
-            {
-                entities.AddRange(segment);
-            }, cancellationToken);
+            var entities = await HackathonManager.SearchHackathonAsync(null, cancellationToken);
+            return Ok(ResponseBuilder.BuildHackathonList(entities));
+        }
 
-            return Ok(entities);
+        /// <summary>
+        /// Create or update hackathon. 
+        /// If id is absent, will create a new hackathon. 
+        /// If id is not empty, will retrive the hackathon(return 404 if not found) and update accordingly.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <param name="cancellation"></param>
+        /// <returns></returns>
+        /// <response code="200">Success. The response describes a hackathon.</response>
+        /// <response code="404">Not Found. The response indicates hackathon with specified id doesn't exist.</response>
+        [HttpPut]
+        [ProducesResponseType(typeof(Hackathon), 200)]
+        public async Task<Hackathon> CreateOrUpdate(Hackathon parameter, CancellationToken cancellation)
+        {
+
         }
     }
 }
---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/ResponseBuilder/DefaultResponseBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Kaiyuanshe.OpenHackathon.Server.Models;
+using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kaiyuanshe.OpenHackathon.Server.ResponseBuilder
+{
+    public interface IResponseBuilder
+    {
+        Hackathon BuildHackathon(HackathonEntity hackathonEntity);
+        IEnumerable<Hackathon> BuildHackathonList(IEnumerable<HackathonEntity> hackathonEntities);
+    }
+
+    public class DefaultResponseBuilder : IResponseBuilder
+    {
+        public Hackathon BuildHackathon(HackathonEntity hackathonEntity)
+        {
+            return new Hackathon
+            {
+                Id = hackathonEntity.Id,
+                AutoApprove = hackathonEntity.AutoApprove,
+                Banners = hackathonEntity.Banners,
+                CreatorId = hackathonEntity.CreatorId,
+                Description = hackathonEntity.Description,
+                Location = hackathonEntity.Location,
+                Name = hackathonEntity.Name,
+                CreatedTime = hackathonEntity.CreatedTime,
+                EnrollmentStartTime = hackathonEntity.EnrollmentStartTime,
+                EnrollmentEndTime = hackathonEntity.EnrollmentEndTime,
+                EventStartTime = hackathonEntity.EventStartTime,
+                EventEndTime = hackathonEntity.EventEndTime,
+                JudgeStartTime = hackathonEntity.JudgeStartTime,
+                JudgeEndTime = hackathonEntity.JudgeEndTime,
+                MaxEnrollment = hackathonEntity.MaxEnrollment,
+                Ribbon = hackathonEntity.Ribbon,
+                Status = hackathonEntity.Status,
+                Summary = hackathonEntity.Summary,
+                Tags = hackathonEntity.Tags
+            };
+        }
+
+        public IEnumerable<Hackathon> BuildHackathonList(IEnumerable<HackathonEntity> hackathonEntities)
+        {
+            return hackathonEntities.Select(h => BuildHackathon(h));
+        }
+    }
+}
