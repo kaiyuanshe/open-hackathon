538c9434d4ec4e363723a5a07fbe444a6ec38aeb
Junbo Wang <juniwang@microsoft.com>
2021-01-19 19:07:58 +0800

    Add default error response to all APIs

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Swagger/OperationFilters.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Swagger/OperationFilters.cs
@@ -9,14 +9,33 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Kaiyuanshe.OpenHackathon.Server.Models;
 
 namespace Kaiyuanshe.OpenHackathon.Server.Swagger
 {
+    /// <summary>
+    /// Add <seealso cref="ErrorResponse"/> to Http responses whose statusCode is >=400.
+    /// </summary>
     public class DefaultResponseOperationFilter : IOperationFilter
     {
+        public static readonly string DefaultCode = "default";
+
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
-            
+            var errorRespSchema = context.SchemaGenerator.GenerateSchema(typeof(ErrorResponse), context.SchemaRepository);
+            foreach (var resp in operation.Responses)
+            {
+                if (int.TryParse(resp.Key, out int httpStatusCode) && httpStatusCode >= 400)
+                {
+                    resp.Value.Content = new Dictionary<string, OpenApiMediaType>
+                    {
+                        ["application/json"] = new OpenApiMediaType
+                        {
+                            Schema = errorRespSchema
+                        }
+                    };
+                }
+            }
         }
     }
 }
