f3b626928b18f379238f37834c0acb140f5754f9
Junbo Wang <juniwang@microsoft.com>
2021-01-22 18:26:04 +0800

    adding entities for user

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Entities/UserTokenEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Storage.Entities
+{
+    /// <summary>
+    /// Token
+    /// 
+    /// PartitionKey: token
+    /// RowKey: string.Empty
+    /// </summary>
+    public class UserTokenEntity : AdvancedTableEntity
+    {
+        public string UserId { get; set; }
+
+        public string Token
+        {
+            get
+            {
+                return PartitionKey;
+            }
+        }
+
+        public DateTime TokenExpiredAt { get; set; }
+    }
+}
