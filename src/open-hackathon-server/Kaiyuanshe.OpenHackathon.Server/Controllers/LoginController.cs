9583b3a782623e5d26b66968c845d0d6dd476b70
Junbo Wang <juniwang@microsoft.com>
2021-01-24 19:23:51 +0800

    adding models for login

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Controllers
+{
+    public class LoginController : HackathonControllerBase
+    {
+        public async Task<object> Authing()
+        {
+            return Ok();
+        }
+    }
+}
