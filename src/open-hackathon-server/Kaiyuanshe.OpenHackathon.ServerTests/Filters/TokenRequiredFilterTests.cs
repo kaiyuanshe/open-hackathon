e7e45d20859bb6eebd2faf35c61ff301ab48e688
Junbo Wang <juniwang@microsoft.com>
2021-02-10 12:24:23 +0800

    UT for TokenRequiredFilter

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Filters/TokenRequiredFilterTests.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Filters/TokenRequiredFilterTests.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Kaiyuanshe.OpenHackathon.Server.Biz;
+using Kaiyuanshe.OpenHackathon.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 using Moq;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
 using static Kaiyuanshe.OpenHackathon.Server.Filters.TokenRequiredAttribute;
 
 namespace Kaiyuanshe.OpenHackathon.ServerTests.Filters
@@ -11,8 +19,128 @@ namespace Kaiyuanshe.OpenHackathon.ServerTests.Filters
     [TestFixture]
     public class TokenRequiredFilterTests
     {
-        //TokenRequiredFilter SetupHttpContext(Action<Mock<HttpContext>> configure = null)
-        //{
-        //}
+        [Test]
+        public void OnActionExecutingTestMissing()
+        {
+            // setup
+            var loginManagerMock = new Mock<ILoginManager>();
+            var httpContextMock = new Mock<HttpContext>();
+            var httpRequestMock = new Mock<HttpRequest>();
+            var headerMock = new Mock<IHeaderDictionary>();
+            var filterMetadataMock = new Mock<IList<IFilterMetadata>>();
+            httpContextMock.SetupGet(h => h.Request).Returns(httpRequestMock.Object).Verifiable();
+            httpRequestMock.SetupGet(h => h.Headers).Returns(headerMock.Object).Verifiable();
+            headerMock.Setup(h => h.ContainsKey(HeaderNames.Authorization)).Returns(false).Verifiable();
+
+            var actionContext = new ActionContext(httpContextMock.Object, new RouteData(), new ActionDescriptor());
+            var context = new ActionExecutingContext(actionContext, filterMetadataMock.Object, new Dictionary<string, object>(), null);
+
+            // test
+            var filter = new TokenRequiredFilter(loginManagerMock.Object);
+            filter.OnActionExecuting(context);
+
+            Mock.VerifyAll(loginManagerMock, httpContextMock, httpRequestMock, headerMock);
+            Assert.IsNotNull(context.Result);
+            Assert.IsTrue(context.Result is ObjectResult);
+            ObjectResult result = (ObjectResult)context.Result;
+            Assert.AreEqual(401, result.StatusCode.GetValueOrDefault(0));
+            Assert.IsTrue(result.Value is ErrorResponse);
+            ErrorResponse error = (ErrorResponse)result.Value;
+            Assert.AreEqual("Unauthorized", error.error.code);
+            Assert.IsTrue(error.error.message.Contains("missing"));
+        }
+
+        [TestCase("abc")]
+        [TestCase("token")]
+        [TestCase("")]
+        public void OnActionExecutingTestMalformat(string tokenValue)
+        {
+            // setup
+            var loginManagerMock = new Mock<ILoginManager>();
+            var httpContextMock = new Mock<HttpContext>();
+            var httpRequestMock = new Mock<HttpRequest>();
+            var headerMock = new Mock<IHeaderDictionary>();
+            var filterMetadataMock = new Mock<IList<IFilterMetadata>>();
+            httpContextMock.SetupGet(h => h.Request).Returns(httpRequestMock.Object).Verifiable();
+            httpRequestMock.SetupGet(h => h.Headers).Returns(headerMock.Object).Verifiable();
+            headerMock.Setup(h => h.ContainsKey(HeaderNames.Authorization)).Returns(true).Verifiable();
+            headerMock.SetupGet(p => p[HeaderNames.Authorization]).Returns(new StringValues(tokenValue));
+
+            var actionContext = new ActionContext(httpContextMock.Object, new RouteData(), new ActionDescriptor());
+            var context = new ActionExecutingContext(actionContext, filterMetadataMock.Object, new Dictionary<string, object>(), null);
+
+            // test
+            var filter = new TokenRequiredFilter(loginManagerMock.Object);
+            filter.OnActionExecuting(context);
+
+            Mock.VerifyAll(loginManagerMock, httpContextMock, httpRequestMock, headerMock);
+            Assert.IsNotNull(context.Result);
+            Assert.IsTrue(context.Result is ObjectResult);
+            ObjectResult result = (ObjectResult)context.Result;
+            Assert.AreEqual(401, result.StatusCode.GetValueOrDefault(0));
+            Assert.IsTrue(result.Value is ErrorResponse);
+            ErrorResponse error = (ErrorResponse)result.Value;
+            Assert.AreEqual("Unauthorized", error.error.code);
+            Assert.IsTrue(error.error.message.Contains("malformatted"));
+        }
+
+        [Test]
+        public void OnActionExecutingTestValidateFailed()
+        {
+            // setup
+            var loginManagerMock = new Mock<ILoginManager>();
+            loginManagerMock.Setup(p => p.ValidateTokenAsync("TOKEN", It.IsAny<CancellationToken>())).ReturnsAsync(new ValidationResult("whatever")).Verifiable();
+            var httpContextMock = new Mock<HttpContext>();
+            var httpRequestMock = new Mock<HttpRequest>();
+            var headerMock = new Mock<IHeaderDictionary>();
+            var filterMetadataMock = new Mock<IList<IFilterMetadata>>();
+            httpContextMock.SetupGet(h => h.Request).Returns(httpRequestMock.Object).Verifiable();
+            httpRequestMock.SetupGet(h => h.Headers).Returns(headerMock.Object).Verifiable();
+            headerMock.Setup(h => h.ContainsKey(HeaderNames.Authorization)).Returns(true).Verifiable();
+            headerMock.SetupGet(p => p[HeaderNames.Authorization]).Returns(new StringValues("token TOKEN"));
+
+            var actionContext = new ActionContext(httpContextMock.Object, new RouteData(), new ActionDescriptor());
+            var context = new ActionExecutingContext(actionContext, filterMetadataMock.Object, new Dictionary<string, object>(), null);
+
+            // test
+            var filter = new TokenRequiredFilter(loginManagerMock.Object);
+            filter.OnActionExecuting(context);
+
+            Mock.VerifyAll(loginManagerMock, httpContextMock, httpRequestMock, headerMock);
+            Assert.IsNotNull(context.Result);
+            Assert.IsTrue(context.Result is ObjectResult);
+            ObjectResult result = (ObjectResult)context.Result;
+            Assert.AreEqual(401, result.StatusCode.GetValueOrDefault(0));
+            Assert.IsTrue(result.Value is ErrorResponse);
+            ErrorResponse error = (ErrorResponse)result.Value;
+            Assert.AreEqual("Unauthorized", error.error.code);
+            Assert.IsTrue(error.error.message.Contains("whatever"));
+        }
+
+        [Test]
+        public void OnActionExecutingTestPassed()
+        {
+            // setup
+            var loginManagerMock = new Mock<ILoginManager>();
+            loginManagerMock.Setup(p => p.ValidateTokenAsync("TOKEN", It.IsAny<CancellationToken>())).ReturnsAsync(ValidationResult.Success).Verifiable();
+            var httpContextMock = new Mock<HttpContext>();
+            var httpRequestMock = new Mock<HttpRequest>();
+            var headerMock = new Mock<IHeaderDictionary>();
+            var filterMetadataMock = new Mock<IList<IFilterMetadata>>();
+            httpContextMock.SetupGet(h => h.Request).Returns(httpRequestMock.Object).Verifiable();
+            httpRequestMock.SetupGet(h => h.Headers).Returns(headerMock.Object).Verifiable();
+            headerMock.Setup(h => h.ContainsKey(HeaderNames.Authorization)).Returns(true).Verifiable();
+            headerMock.SetupGet(p => p[HeaderNames.Authorization]).Returns(new StringValues("token TOKEN"));
+
+            var actionContext = new ActionContext(httpContextMock.Object, new RouteData(), new ActionDescriptor());
+            var context = new ActionExecutingContext(actionContext, filterMetadataMock.Object, new Dictionary<string, object>(), null);
+
+            // test
+            var filter = new TokenRequiredFilter(loginManagerMock.Object);
+            filter.OnActionExecuting(context);
+
+            Mock.VerifyAll(loginManagerMock, httpContextMock, httpRequestMock, headerMock);
+            Assert.IsNull(context.Result);
+        }
     }
 }
