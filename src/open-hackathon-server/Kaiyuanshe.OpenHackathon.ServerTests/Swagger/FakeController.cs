7f86c91323677e87850300933d86812f12d63586
Junbo Wang <juniwang@microsoft.com>
2021-02-18 11:15:53 +0800

    clean up filters (#886)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Swagger/FakeController.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Swagger/FakeController.cs
@@ -1,5 +1,4 @@
-﻿using Kaiyuanshe.OpenHackathon.Server.Filters;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
