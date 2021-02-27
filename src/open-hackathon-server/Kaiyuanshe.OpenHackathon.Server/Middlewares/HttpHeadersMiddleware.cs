734b8b63496dd8c3144058af8eefe7ac492913e9
Junbo Wang <juniwang@microsoft.com>
2021-02-27 22:03:54 +0800

    log part 1: middleware and traceid

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Middlewares/HttpHeadersMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Middlewares
+{
+    public class HttpHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("traceid", Activity.Current?.Id ?? string.Empty);
+            return _next(context);
+        }
+    }
+}
