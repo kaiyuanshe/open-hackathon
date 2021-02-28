47e65bb0d49396a4e24027e3a8a87b8d7496e545
Junbo Wang <juniwang@microsoft.com>
2021-02-28 20:39:37 +0800

    logging part 2: log trace id (#901)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Extensions/LoggerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server
+{
+    public static class LoggerExtensions
+    {
+        public static void TraceInformation(this ILogger logger, string message)
+        {
+            string traceId = Activity.Current?.Id ?? string.Empty;
+            string messageWithTraceId = $"[{traceId}] {message}";
+            logger.LogInformation(messageWithTraceId);
+        }
+
+        public static void TraceError(this ILogger logger, string message, Exception exception)
+        {
+            string traceId = Activity.Current?.Id ?? string.Empty;
+            string messageWithTraceId = $"[{traceId}] {message}";
+            logger.LogError(exception, messageWithTraceId);
+        }
+    }
+}
