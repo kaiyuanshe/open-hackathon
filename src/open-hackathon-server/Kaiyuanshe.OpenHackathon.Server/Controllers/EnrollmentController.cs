8c4650b394da6a7070e31b2bb56b27b41c94186f
Junbo Wang <juniwang@microsoft.com>
2021-03-14 22:03:42 +0800

    controller for enroll

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/EnrollmentController.cs
@@ -0,0 +1,50 @@
+using Kaiyuanshe.OpenHackathon.Server.Auth;
+using Kaiyuanshe.OpenHackathon.Server.Biz;
+using Kaiyuanshe.OpenHackathon.Server.Models;
+using Kaiyuanshe.OpenHackathon.Server.ResponseBuilder;
+using Kaiyuanshe.OpenHackathon.Server.Storage;
+using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Controllers
+{
+    public class EnrollmentController : HackathonControllerBase
+    {
+        public IHackathonManagement HackathonManagement { get; set; }
+
+        /// <summary>
+        /// Enroll a hackathon.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <param name="hackathonName" example="foo">Name of hackathon. Case-insensitive.
+        /// Must contain only letters and/or numbers, length between 1 and 100</param>
+        /// <param name="userId" example="1">unique id of the login user</param>
+        /// <returns></returns>
+        /// <response code="200">Success. The response describes a enrollment.</response>
+        /// <response code="400">Bad Reqeuest. The response indicates the client request is not valid.</response>
+        /// <response code="404">Not Found. The response indicates the hackathon or user is not found.</response>
+        [HttpPut]
+        [ProducesResponseType(typeof(Enrollment), StatusCodes.Status200OK)]
+        [Route("hackathon/{hackathonName}/enrollment/{userId}")]
+        [Authorize]
+        public async Task<object> Enroll(
+            [FromRoute, Required, RegularExpression(ModelConstants.HackathonNamePattern)] string hackathonName,
+            [FromRoute, Required] string userId,
+            [FromBody] Enrollment parameter)
+        {
+            HackathonEntity hackathon = await HackathonManagement.GetHackathonEntityByNameAsync(hackathonName);
+            if (hackathon == null)
+            {
+                return NotFound(string.Format(Resources.Hackathon_NotFound, hackathonName));
+            }
+
+            return null;
+        }
+    }
+}
--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/Enrollment.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/Enrollment.cs
@@ -3,18 +3,34 @@ namespace Kaiyuanshe.OpenHackathon.Server.Models
     /// <summary>
     /// Status of the enrollment
     /// </summary>
+    public class Enrollment
+    {
+        /// <summary>
+        /// Status of enrollment.
+        /// </summary>
+        public EnrollmentStatus status { get; internal set; }
+    }
+
+    /// <summary>
+    /// Status of the enrollment
+    /// </summary>
     public enum EnrollmentStatus
     {
+        /// <summary>
+        /// Not enrolled
+        /// </summary>
+        None,
+
         /// <summary>
         /// Pending approval
         /// </summary>
         Pending,
-        
+
         /// <summary>
         /// Approved by admin
         /// </summary>
         Approved,
-        
+
         /// <summary>
         /// Rejected by admin
         /// </summary>
