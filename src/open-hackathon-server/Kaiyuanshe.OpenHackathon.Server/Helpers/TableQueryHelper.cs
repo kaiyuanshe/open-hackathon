2160f518e0bc764af5ae0f51520e124f4995de5a
Junbo Wang <juniwang@microsoft.com>
2021-04-02 09:58:23 +0800

    adding table query helper (#928)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Helpers/TableQueryHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Helpers
+{
+    public static class TableQueryHelper
+    {
+        public static string And(params string[] filters)
+        {
+            if (filters == null || filters.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            if (filters.Length == 1)
+                return filters[0];
+
+            string combined = filters[0];
+            for (int i = 1; i < filters.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(filters[i]))
+                {
+                    combined = TableQuery.CombineFilters(combined, TableOperators.And, filters[i]);
+                }
+            }
+            return combined;
+        }
+    }
+}
