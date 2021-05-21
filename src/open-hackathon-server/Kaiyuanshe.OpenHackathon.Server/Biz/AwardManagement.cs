4f1aff8bc07ff8782a4e084af25bea5062cab3c8
Junbo Wang <juniwang@microsoft.com>
2021-05-21 09:19:42 +0800

    get award by id biz

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/AwardManagement.cs
@@ -0,0 +1,41 @@
+﻿using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Biz
+{
+    public interface IAwardManagement
+    {
+        /// <summary>
+        /// Get a Award by Id
+        /// </summary>
+        /// <param name="hackathonName">name of hackathon</param>
+        /// <param name="awardId">unique id of the award</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<AwardEntity> GeAwardByIdAsync(string hackathonName, string awardId, CancellationToken cancellationToken = default);
+    }
+
+
+    public class AwardManagement : ManagementClientBase, IAwardManagement
+    {
+        private readonly ILogger Logger;
+
+        public AwardManagement(ILogger<AwardManagement> logger)
+        {
+            Logger = logger;
+        }
+
+        public async Task<AwardEntity> GeAwardByIdAsync(string hackathonName, string awardId, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(hackathonName) || string.IsNullOrWhiteSpace(awardId))
+                return null;
+
+            return await StorageContext.AwardTable.RetrieveAsync(hackathonName.ToLower(), awardId, cancellationToken);
+        }
+    }
+}
