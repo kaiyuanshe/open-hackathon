4929f41cd39a203f6bff07360dea92c923f4f430
Junbo Wang <juniwang@microsoft.com>
2021-05-29 20:12:00 +0800

    api to list awards (#990)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/AwardController.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Controllers/AwardController.cs
@@ -2,10 +2,12 @@
 using Kaiyuanshe.OpenHackathon.Server.Biz;
 using Kaiyuanshe.OpenHackathon.Server.Models;
 using Kaiyuanshe.OpenHackathon.Server.ResponseBuilder;
+using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
 using Kaiyuanshe.OpenHackathon.Server.Swagger;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
@@ -142,6 +144,54 @@ namespace Kaiyuanshe.OpenHackathon.Server.Controllers
             // update award
             var updated = await AwardManagement.UpdateAwardAsync(award, parameter, cancellationToken);
             return Ok(ResponseBuilder.BuildAward(updated));
+        }
+        #endregion
+
+        #region ListAwards
+        /// <summary>
+        /// List paginated awards of a hackathon.
+        /// </summary>
+        /// <param name="hackathonName" example="foo">Name of hackathon. Case-insensitive.
+        /// Must contain only letters and/or numbers, length between 1 and 100</param>
+        /// <returns>the response contains a list of awards and a nextLink if there are more results.</returns>
+        /// <response code="200">Success.</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(AwardList), StatusCodes.Status200OK)]
+        [SwaggerErrorResponse(400, 404)]
+        [Route("hackathon/{hackathonName}/awards")]
+        public async Task<object> ListAwards(
+            [FromRoute, Required, RegularExpression(ModelConstants.HackathonNamePattern)] string hackathonName,
+            [FromQuery] Pagination pagination,
+            CancellationToken cancellationToken)
+        {
+            // validate hackathon
+            var hackathon = await HackathonManagement.GetHackathonEntityByNameAsync(hackathonName.ToLower());
+            var options = new ValidateHackathonOptions
+            {
+                HackathonName = hackathonName,
+            };
+            if (await ValidateHackathon(hackathon, options) == false)
+            {
+                return options.ValidateResult;
+            }
+
+            // query
+            var awardQueryOptions = new AwardQueryOptions
+            {
+                TableContinuationToken = pagination.ToContinuationToken(),
+                Top = pagination.top
+            };
+            var segment = await AwardManagement.ListPaginatedAwardsAsync(hackathonName.ToLower(), awardQueryOptions, cancellationToken);
+            var routeValues = new RouteValueDictionary();
+            if (pagination.top.HasValue)
+            {
+                routeValues.Add(nameof(pagination.top), pagination.top.Value);
+            }
+            var nextLink = BuildNextLinkUrl(routeValues, segment.ContinuationToken);
+            return Ok(ResponseBuilder.BuildResourceList<AwardEntity, Award, AwardList>(
+                    segment,
+                    ResponseBuilder.BuildAward,
+                    nextLink));
         }
         #endregion
     }
