d3be3e074b5e6dd6085afb93bab8f9a5964b64ea
think-in-universe <mstr.raphael@gmail.com>
2021-07-05 01:34:11 +0800

    update sas url example

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/SasUrl.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/SasUrl.cs
@@ -11,7 +11,7 @@ namespace Kaiyuanshe.OpenHackathon.Server.Models
         /// <summary>
         /// SAS URL
         /// </summary>
-        /// <example>?sv=2018-03-28&amp;sr=b&amp;sig=JT1H%2FcjWK1xbwK3gy1qXXbXAxuQBTmYoCwpmK2Z9Ls0%3D&amp;st=2021-07-04T09%3A25%3A39Z&amp;se=2021-07-04T09%3A32%3A39Z&amp;sp=rw"</example>
+        /// <example>https://container.blob.core.chinacloudapi.cn/kysuser/user/avatar.jpeg?sv=2018-03-28&amp;sr=b&amp;sig=JT1H%2FcjWK1xbwK3gy1qXXbXAxuQBTmYoCwpmK2Z9Ls0%3D&amp;st=2021-07-04T09%3A25%3A39Z&amp;se=2021-07-04T09%3A32%3A39Z&amp;sp=rw</example>
         [JsonProperty("url")]
         public string url { get; set; }
     }
