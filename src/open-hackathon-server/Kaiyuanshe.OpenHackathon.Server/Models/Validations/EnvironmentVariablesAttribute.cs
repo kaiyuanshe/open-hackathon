b365a37576f191f2c8f947c841a866c82d2fb04f
Junbo Wang <wangjunbo924@hotmail.com>
2021-09-02 22:16:32 +0800

    add model for hackathon template

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/Validations/EnvironmentVariablesAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Models.Validations
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EnvironmentVariablesAttribute : ValidationAttribute
+    {
+        private static readonly int NameMaxLength = 64;
+        private static readonly int ValueMaxLength = 256;
+        private static readonly int MaxEnvs = 32;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                // leave it to Required
+                return ValidationResult.Success;
+            }
+
+            Dictionary<string, string> envs = value as Dictionary<string, string>;
+            if (envs.Count > MaxEnvs)
+            {
+                return new ValidationResult($"a maximum of {MaxEnvs} environment variables are allowed.");
+            }
+
+            List<string> invalidMembers = new List<string>();
+            invalidMembers.AddRange(envs.Keys.Where(k => k.Length > NameMaxLength));
+            invalidMembers.AddRange(envs.Values.Where(k => k.Length > ValueMaxLength));
+            if (invalidMembers.Count > 0)
+            {
+                string msg = $"For every env variable, the name must not exeed {NameMaxLength} in length, and the value must not exceed {ValueMaxLength} in length.";
+                return new ValidationResult(msg, invalidMembers);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Models/Validations/EnvironmentVariablesAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using Kaiyuanshe.OpenHackathon.Server.Models.Validations;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Kaiyuanshe.OpenHackathon.ServerTests.Models.Validations
+{
+    class EnvironmentVariablesAttributeTests
+    {
+        [Test]
+        public void IsValidTest()
+        {
+            var attribute = new EnvironmentVariablesAttribute();
+            Assert.AreEqual(true, attribute.IsValid(null));
+
+            // max number
+            Dictionary<string, string> a = new Dictionary<string, string>();
+            for (int i = 0; i < 32; i++)
+            {
+                a[i.ToString()] = i.ToString();
+            }
+            Assert.AreEqual(true, attribute.IsValid(a));
+            a["more"] = "more";
+            Assert.AreEqual(false, attribute.IsValid(a));
+
+            // invalid name/value
+            Dictionary<string, string> b = new Dictionary<string, string>
+            {
+                { new string('a', 65), "a"}
+            };
+            Assert.AreEqual(false, attribute.IsValid(b));
+
+            Dictionary<string, string> c = new Dictionary<string, string>
+            {
+                {  "a",new string('a', 257)}
+            };
+            Assert.AreEqual(false, attribute.IsValid(c));
+        }
+    }
+}
