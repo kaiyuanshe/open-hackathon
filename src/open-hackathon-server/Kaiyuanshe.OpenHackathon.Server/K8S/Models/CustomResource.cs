bfb4f47aa5610074d762ad6a84a7f0ada30e3b8d
Junbo Wang <juniwang@microsoft.com>
2021-09-10 22:12:08 +0800

    add custom resource for template (#1111)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/K8S/Models/CustomResource.cs
@@ -0,0 +1,21 @@
+﻿using k8s;
+using k8s.Models;
+using Newtonsoft.Json;
+
+namespace Kaiyuanshe.OpenHackathon.Server.K8S.Models
+{
+    public abstract class CustomResource : KubernetesObject
+    {
+        [JsonProperty(PropertyName = "metadata")]
+        public V1ObjectMeta Metadata { get; set; }
+    }
+
+    public abstract class CustomResource<TSpec, TStatus> : CustomResource
+    {
+        [JsonProperty(PropertyName = "spec")]
+        public TSpec Spec { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public TStatus CStatus { get; set; }
+    }
+}
---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/K8S/Models/TemplateResource.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Kaiyuanshe.OpenHackathon.Server.K8S.Models
+{
+    public class TemplateResource : CustomResource
+    {
+        // Properties of CustomResourceDefinition
+        // See also: https://github.com/kaiyuanshe/cloudengine/blob/master/config/crd/bases/hackathon.kaiyuanshe.cn_templates.yaml
+        public static readonly string Group = "hackathon.kaiyuanshe.cn";
+        public static readonly string Version = "v1";
+        public static readonly string Plural = "templates";
+
+        [JsonProperty(PropertyName = "data")]
+        public TemplateData Data { get; set; }
+    }
+
+    public class TemplateData
+    {
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "podTemplate")]
+        public PodTemplate PodTemplate { get; set; }
+
+        [JsonProperty(PropertyName = "ingressProtocol")]
+        public string IngressProtocol { get; set; }
+
+        [JsonProperty(PropertyName = "ingressPort")]
+        public string IngressPort { get; set; }
+
+        [JsonProperty(PropertyName = "vnc")]
+        public VncConnection VncConnection { get; set; }
+    }
+
+    public class PodTemplate
+    {
+        [JsonProperty(PropertyName = "image")]
+        public string Image { get; set; }
+
+        [JsonProperty(PropertyName = "command")]
+        public string[] Command { get; set; }
+
+        [JsonProperty(PropertyName = "env")]
+        public IDictionary<string, string> EnvironmentVariables { get; set; }
+    }
+
+    public class VncConnection
+    {
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+    }
+}
